87a47bf357a2a4210c36f125b47a79036e756e3f
RobertDynell <robert.haslinger@dynell.at>
2024-08-01 07:57:10 +0200

    no message

--- cross-arm-linux-gnueabihf.cmake
+++ cross-arm-linux-gnueabihf.cmake
@@ -47,5 +47,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 #build:
 # mkdir build
 # cd build
-# cmake . -DCMAKE_TOOLCHAIN_FILE=./cross-arm-linux-gnueabihf.cmake -DLWS_WITH_SSL=OFF -DBUILD_SHARED_LIBS=Off --fresh --debug-trycompile
+# cmake . -DCMAKE_TOOLCHAIN_FILE=./cross-arm-linux-gnueabihf.cmake -DLWS_WITH_SSL=OFF -DBUILD_SHARED_LIBS=OFF --fresh --debug-trycompile
 # make
